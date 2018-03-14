49bb547cf611d0af24a99d1e1c02ffc8692f8230
Nodiraj <raytsentr@gmail.com>
2018-03-14 12:36:33 -0400

    new messege

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -5,5 +5,6 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good evening");
 		System.out.println("she is Maria");
+		System.out.println("new messege");
 	}
 }
